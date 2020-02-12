cf196af3edaee8ca93a11312a54a7d457a4c21e7
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-12 16:45:29 -0300

    add Should show a message when username length less than 3 characters

--- tests/e2e/specs/login.spec.js
+++ tests/e2e/specs/login.spec.js
@@ -8,30 +8,47 @@ const xpathSection = '//section[contains(@class, "login")]'
 const xpathUsername = `${xpathSection}//*[@id="username"]`
 const xpathPassword = `${xpathSection}//*[@id="password"]`
 const xpathLogin = `${xpathSection}//*[@id="login"]`
+const xpathErrorMinLength = '//div[contains(@class, "v-messages__message") and contains(text(), "Insira pelo menos 3 caracteres")]'
 
 module.exports = {
   beforeEach: (browser) => {
     browser
       .refresh()
       .waitForElementVisible(xpathSection)
+      .waitForElementVisible(xpathUsername)
+      .waitForElementVisible(xpathPassword)
   },
 
   'Should disabled login button when username and password is null': function (browser) {
     browser
-      .waitForElementVisible(xpathUsername)
-      .waitForElementVisible(xpathPassword)
       .assert.disabledProp(xpathLogin, true)
   },
 
   'Should disabled login button when username is null': function (browser) {
     browser
-      .waitForElementVisible(xpathUsername)
+      .sendKeys(xpathPassword, 'a')
       .assert.disabledProp(xpathLogin, true)
   },
 
   'Should disabled login button when password is null': function (browser) {
     browser
-      .waitForElementVisible(xpathPassword)
+      .sendKeys(xpathUsername, 'a')
       .assert.disabledProp(xpathLogin, true)
+  },
+
+  'Should enable login button when username and password is not null': function (browser) {
+    browser
+      .sendKeys(xpathUsername, 'a')
+      .sendKeys(xpathPassword, 'a')
+      .assert.disabledProp(xpathLogin, false)
+  },
+
+  'Should show a message when username length less than 3 characters': function (browser) {
+    browser
+      .sendKeys(xpathUsername, 'a')
+      .sendKeys(xpathPassword, 'a')
+      .click(xpathLogin)
+      .waitForElementVisible(xpathErrorMinLength)
+      .expect.element(xpathErrorMinLength).to.be.visible
   },
 }
