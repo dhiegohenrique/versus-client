b0ebe9e02d21994112f695951e8fae15e2b4c547
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-12 20:46:37 -0300

    add Should logout when back on navigation

--- tests/e2e/specs/logout.spec.js
+++ tests/e2e/specs/logout.spec.js
@@ -1,21 +1,38 @@
 const waitTime = 60000 * 2
 const xpathSectionLogin = '//section[contains(@class, "login")]'
+const xpathLogout = '//*[@id="logout"]'
 
 module.exports = {
   beforeEach: (browser) => {
-    const loginPage = browser.page.login()
-    loginPage.login()
+    browser
+      .perform(() => {
+        const loginPage = browser.page.login()
+        loginPage.login()
+      })
+      .perform(async () => {
+        await browser.waitForElementVisible(xpathLogout)
+      })
   },
 
-  'Should logout when click in logout button': async function (browser) {
-    const xpathLogout = '//*[@id="logout"]'
-
-    await browser.waitForElementVisible(xpathLogout)
+  'Should logout when click in logout button': !async function (browser) {
     await browser.click(xpathLogout)
+    await validateLoginPage(browser)
+    await validateLocalStorage(browser)
+  },
+
+  'Should logout when back on navigation': async function (browser) {
+    await browser.back()
+    await validateLoginPage(browser)
+    await validateLocalStorage(browser)
+  },
+}
+
+const validateLoginPage = (browser) => {
+  return new Promise(async (resolve) => {
     await browser.waitForElementVisible(xpathSectionLogin)
     await browser.expect.element(xpathSectionLogin).to.be.visible
-    await validateLocalStorage(browser)
-  }
+    resolve()
+  })
 }
 
 const validateLocalStorage = (browser) => {
