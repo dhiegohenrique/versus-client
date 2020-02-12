e5457a99fab14598213c8e50c4a5bf13c1279ae8
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-12 10:05:15 -0300

    add authenticated route

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
+import AuthService from '@/shared/services/auth.service'
 
 Vue.use(VueRouter)
 
+const authService = AuthService()
 const router = new VueRouter({ mode: 'history', routes })
 
 router.beforeEach((to, from, next) => {
-  // if (to.meta.requiresAuth && !authService.isAuthenticated()) {
-  //   return next('/')
-  // }
+  if (to.meta.requiresAuth && !authService.isAuthenticated()) {
+    return next('/')
+  }
 
   next()
 })
