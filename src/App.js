912cc0745d945bd5a50617be6de244001ca7fc7f
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-12 10:09:51 -0300

    remove watch

--- src/App.js
+++ src/App.js
@@ -8,18 +8,5 @@ export default {
     LoadingModal,
     Toast,
     Logo
-  },
-  watch: {
-    $route: function (to, from) {
-      // eslint-disable-next-line no-console
-      console.log('to: ' + to.name + ', from: ' + from.name)
-      // if (/login/.test(to.name)) {
-      //   this.hideUtilities()
-      // }
-
-      // if (/login/.test(from.name)) {
-      //   this.showUtilities()
-      // }
-    }
   }
 }
