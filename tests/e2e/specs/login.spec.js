d086a27452ea9345763b861ad27d0707710c74fa
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-12 19:10:36 -0300

    add Should show home when login is successful

--- tests/e2e/specs/login.spec.js
+++ tests/e2e/specs/login.spec.js
@@ -4,6 +4,8 @@ const xpathPassword = `${xpathSection}//*[@id="password"]`
 const xpathLogin = `${xpathSection}//*[@id="login"]`
 const xpathErrorMinLength = '//div[contains(@class, "v-messages__message") and contains(text(), "Insira pelo menos 3 caracteres")]'
 const xpathToast = `//div[contains(@class, "toasted-container")]//div[contains(@class, "toasted") and contains(text(), "Username ou senha incorretos.")]//i[contains(text(), "info")]`
+const username = 'ViRRO'
+const password = '12345678'
 
 module.exports = {
   beforeEach: (browser) => {
@@ -14,49 +16,66 @@ module.exports = {
       .waitForElementVisible(xpathPassword)
   },
 
-  'Should disabled login button when username and password is null': !function (browser) {
+  'Should disabled login button when username and password is null': function (browser) {
     browser
       .assert.disabledProp(xpathLogin, true)
   },
 
-  'Should disabled login button when username is null': !function (browser) {
+  'Should disabled login button when username is null': function (browser) {
     browser
       .sendKeys(xpathPassword, 'a')
       .assert.disabledProp(xpathLogin, true)
   },
 
-  'Should disabled login button when password is null': !function (browser) {
+  'Should disabled login button when password is null': function (browser) {
     browser
       .sendKeys(xpathUsername, 'a')
       .assert.disabledProp(xpathLogin, true)
   },
 
-  'Should enable login button when username and password is not null': !function (browser) {
+  'Should enable login button when username and password is not null': function (browser) {
     browser
       .sendKeys(xpathUsername, 'a')
       .sendKeys(xpathPassword, 'a')
       .assert.disabledProp(xpathLogin, false)
   },
 
-  'Should show a message when username length less than 3 characters': !async function (browser) {
+  'Should show a message when username length less than 3 characters': async function (browser) {
     await browser.sendKeys(xpathPassword, 'abcd')
     await validateMinLength(browser, xpathUsername)
   },
 
-  'Should show a message when password length less than 3 characters': !async function (browser) {
+  'Should show a message when password length less than 3 characters': async function (browser) {
     await browser.sendKeys(xpathUsername, 'abcd')
     await validateMinLength(browser, xpathPassword)
   },
 
-  'Should show a message when password is incorrect': !async function (browser) {
-    await browser.sendKeys(xpathUsername, 'ViRRO')
+  'Should show a message when password is incorrect': async function (browser) {
+    await browser.sendKeys(xpathUsername, username)
     await validateUsernameOrPassword(browser, xpathPassword)
   },
 
   'Should show a message when username is incorrect': async function (browser) {
-    await browser.sendKeys(xpathPassword, '12345678')
+    await browser.sendKeys(xpathPassword, password)
     await validateUsernameOrPassword(browser, xpathUsername)
   },
+
+  'Should show a message when username is incorrect': async function (browser) {
+    await browser.sendKeys(xpathPassword, password)
+    await validateUsernameOrPassword(browser, xpathUsername)
+  },
+
+  'Should show home when login is successful': function (browser) {
+    const xpathHome = '//section[contains(@class, "home")]'
+
+    browser
+      .sendKeys(xpathUsername, username)
+      .sendKeys(xpathPassword, password)
+      .click(xpathLogin)
+      .waitForLoadingModal()
+      .waitForElementVisible(xpathHome)
+      .expect.element(xpathHome).to.be.visible
+  }
 }
 
 const validateMinLength = (browser, xpath) => {
